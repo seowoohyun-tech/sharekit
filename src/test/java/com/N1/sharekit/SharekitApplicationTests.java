20c93a669650e53c2e193dd6bce85ef282f273b7
seowoohyun-tech <202403589@hufs.ac.kr>
2025-05-20 19:55:30 +0900

    initial commit

---
+++ src/test/java/com/N1/sharekit/SharekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.N1.sharekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
