9fc1343c0eddd1a67e984a90036760ec4d1f1873
seowoohyun-tech <202403589@hufs.ac.kr>
2025-05-23 15:54:43 +0900

    rename N1 to niceone

---
+++ backend/src/main/java/com/N1/sharekit/SharekitApplication.java
@@ -0,0 +1,12 @@
+package com.N1.sharekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharekitApplication.class, args);
+	}
+}
---
+++ src/main/java/com/niceone/sharekit/SharekitApplication.java
@@ -0,0 +1,12 @@
+package com.niceone.sharekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharekitApplication.class, args);
+	}
+}
