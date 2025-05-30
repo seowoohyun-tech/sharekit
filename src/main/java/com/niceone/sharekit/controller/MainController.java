7009c0407b901a2887cb9a5d2c3dcf816f274992
rkawkzpark <hungrygamja0106@gmail.com>
2025-05-30 09:27:05 +0900

    feat: Implement basic static page routing controller

---
+++ src/main/java/com/niceone/sharekit/controller/MainController.java
@@ -0,0 +1,36 @@
+package com.niceone.sharekit.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @GetMapping("/profile")
+    public String memberInfoPage() {
+        return "profile";
+    }
+
+    @GetMapping("/equipment-list")
+    public String equipmentListPage() {
+        return "equipment-list";
+    }
+}
